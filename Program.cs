d861d62e089348b4447b3c5128a69d28e02b3925
Brandon <esolutions0510@gmail.com>
2020-10-09 01:35:17 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello: ");
+            Console.WriteLine("Brandon Mochel");
+            Console.Write("Your Turn!: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Nice to meet you " + name);
+        }
+    }
+}
